dbc8b5b88c5a4435f0d8633971cb354cbc76ed3f
dnishimoto <dnishimoto@listensoftware.com>
2020-07-03 05:54:13 -0600

    added animations

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
 import 'appBar.dart';
+//import 'dart:math';
+import '../styles.dart';
 
 class HomeWidget extends StatefulWidget {
   HomeWidget({Key key}) : super(key: key);
+
+
 
   @override
   _HomeWidgetState createState() => _HomeWidgetState();
 }
 
-class _HomeWidgetState extends State<HomeWidget> {
+class _HomeWidgetState extends State<HomeWidget> with SingleTickerProviderStateMixin {
+
+    
+Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller=AnimationController(duration: const Duration(seconds: 5), vsync: this);
+
+    animation = Tween<double>(begin: 0, end: 600).animate(controller)..addListener(() {            
+    setState(() { }); 
+    });      
+     //animation = CurvedAnimation(  parent: controller,  curve: Curves.easeIn,  reverseCurve: Curves.easeOut,);
+    controller.forward();
+
+    //animation=controller.drive(  CurveTween(curve: Curves.ease),);
+  }
+ void dispose()
+ {
+    controller.dispose();            
+    super.dispose();        
+ }
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +45,25 @@ class _HomeWidgetState extends State<HomeWidget> {
         backgroundColor: Colors.white,
         body: 
 
-        Row(children: <Widget>[
-          Container(padding: EdgeInsets.all(20.0) ,width:400, child: Image(image: AssetImage('/images/c_sharp.jpg'), fit:BoxFit.fitWidth)),
+        
+          Stack(
+            children:[
+            AnimatedPositioned(
+              duration: Duration(microseconds: 500),
+              top:600-animation.value,
+              left:600-animation.value,
+              child:Container(padding: EdgeInsets.all(20.0) ,width:400, child: Image(image: AssetImage('/images/c_sharp.jpg'), fit:BoxFit.fitWidth))
+              //FlatButton(onPressed: (){},color: Colors.red,child:Text(""),)
+              ,
+            )
+           ,AnimatedPositioned(
+             duration: Duration(microseconds: 5000),
+             left:450, top:-450+animation.value, child:Text("Meeting all your business needs",style:CustomImageFont))
+          ]
+          ),
          
-          Text("Software for all your needs") 
 
-        ],)
+         
         
         
     );
